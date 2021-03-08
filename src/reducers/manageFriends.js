1ae9bc5dbdbec29dd1d62a10ee4f1eac29f5c6f5
erikn775 <erikn775@gmail.com>
2021-03-08 04:28:29 -0700

    commit tree bfb5419e418dca467ebc472ec5cc7bcd2864d254

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+          return {...friend, friends: state}
+        case 'REMOVE_FRIEND':
+            return {}
+        default:
+            return state;
+    }
 }
